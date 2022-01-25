b7540c4daac2342e972322d494fcd84ee4a67869
dkingapx <dking@apx.com>
2022-01-24 21:39:49 -0500

    my mark up for class

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
-// Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = "right"
+header.style.color = "purple"
+
+//Images
+let dogImages = document.querySelectorAll('.dog-image')
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.borderColor = "green"
+    dogImages[i].style.borderLeftWidth = "10px"
+}
+
+// Next 
+let dogName = document.querySelectorAll('.dog-name')
+for (let i = 0; i < dogName.length; i++) {
+    dogName[i].style.textAlign = "left"
+}
+
+//let dogName = document.querySelectorAll('.dog-name')
+for (let i = 0; i < dogName.length; i++) {
+    dogName[i].style.color = "green"
+}
+//Footer 
+let footer = document.querySelector('.footer')
+footer.style.backgroundColor = 'red'
